ab77f053c2a5d1da8eda6a491eb9de1e7bdfdbad
Ahmed Anwar <74016786+ahmedanwar1@users.noreply.github.com>
2021-05-11 09:43:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,124 @@
+const cheerio = require("cheerio");
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  date = req.query.date; //2021-05-9
+  getMatches(date).then((result) => res.send(result));
+});
+
+app.listen(PORT);
+
+const getMatches = async (date) => {
+  const URL = date
+    ? `https://onefootball.com/en/matches?date=${encodeURIComponent(date)}`
+    : `https://onefootball.com/en/matches`;
+  const response = await axios.get(URL);
+
+  const $ = cheerio.load(response.data);
+  let league = "";
+  matches = [];
+
+  $(
+    ".xpa-layout-matches__section.xpa-layout-matches__section--matchCardsList"
+  ).each((a, element1) => {
+    league = $(element1)
+      .find(".simple-match-cards-list__header span.screen-reader-only")
+      .html();
+    $(element1)
+      .find(`li.simple-match-cards-list__match-card`)
+      .each((i, element2) => {
+        $(element2).each((j, el3) => {
+          matches.push({
+            league,
+            ...getTeamsData($, el3),
+            ...getMatchesTimes($, el3),
+          });
+        });
+      });
+  });
+  return matches;
+};
+
+const getTeamsData = ($, element) => {
+  const teamHome = $(element)
+    .find(
+      `div.simple-match-card__teams-content of-simple-match-card-team.simple-match-card__team-content span.title-8-medium.simple-match-card-team__name`
+    )
+    .html();
+
+  const teamHomeScore = $(element)
+    .find(
+      `div.simple-match-card__teams-content of-simple-match-card-team.simple-match-card__team-content span.title-7-bold.simple-match-card-team__score`
+    )
+    .html();
+
+  const teamHomeLogo = $(element)
+    .find(
+      `div.simple-match-card__teams-content of-simple-match-card-team.simple-match-card__team-content of-entity-logo.simple-match-card-team__logo .of-image__picture`
+    )
+    .children()
+    .attr("srcset");
+
+  const teamAway = $(element)
+    .find(
+      `div.simple-match-card__teams-content of-simple-match-card-team.simple-match-card__team-content:nth-of-type(2) span.title-8-medium.simple-match-card-team__name 
+          `
+    )
+    .html();
+
+  const teamAwayScore = $(element)
+    .find(
+      `div.simple-match-card__teams-content of-simple-match-card-team.simple-match-card__team-content:nth-of-type(2) span.title-7-bold.simple-match-card-team__score 
+          `
+    )
+    .html();
+
+  const teamAwayLogo = $(element)
+    .find(
+      `div.simple-match-card__teams-content of-simple-match-card-team.simple-match-card__team-content:nth-of-type(2) of-entity-logo.simple-match-card-team__logo .of-image__picture`
+    )
+    .children()
+    .attr("srcset");
+
+  return {
+    teamHome,
+    teamHomeScore,
+    teamHomeLogo,
+    teamAway,
+    teamAwayScore,
+    teamAwayLogo,
+  };
+};
+
+const getMatchesTimes = ($, element) => {
+  if (
+    $(element)
+      .find(
+        `.simple-match-card__match-content .title-8-bold.simple-match-card__live`
+      )
+      .html()
+  ) {
+    return {
+      liveScore: $(element)
+        .find(
+          `.simple-match-card__match-content .title-8-bold.simple-match-card__live`
+        )
+        .html(),
+    };
+  } else {
+    return {
+      matchDate: $(element)
+        .find(`.simple-match-card__match-content time`)
+        .html(),
+      status: $(element)
+        .find(
+          `.simple-match-card__match-content span.title-8-regular.simple-match-card__info-message`
+        )
+        .html(),
+    };
+  }
+};
